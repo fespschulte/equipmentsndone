7b57589ad99f285aed5be4460e866838723bc56c
Fernando Pinto <fschultepinto@gmail.com>
2022-03-13 12:27:42 -0300

    criação do footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { EquipmentListComponent } from './equipment-list/equipment-list.component';
 import { EquipmentCardComponent } from './equipment-card/equipment-card.component';
 import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  declarations: [	
+  declarations: [		
     AppComponent,
       EquipmentListComponent,
       EquipmentCardComponent,
-      HeaderComponent
+      HeaderComponent,
+      FooterComponent
    ],
   imports: [
     BrowserModule
